5c921b19b4666d8481bd506e63ca286d68cbfd96
mustafos <mustafos1999@icloud.com>
2024-05-20 17:42:46 +0200

    init project

---
+++ GateOfOlympus/GateOfOlympus/GateOfOlympusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GateOfOlympusApp.swift
+//  GateOfOlympus
+//
+//  Created by Mustafa Bekirov on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GateOfOlympusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
