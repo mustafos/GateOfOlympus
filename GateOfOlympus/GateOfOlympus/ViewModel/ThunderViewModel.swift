f52cbe463a0d9f7514ffa25101f106aa7072b5c9
mustafos <mustafos1999@icloud.com>
2024-06-13 15:04:59 +0200

    docs: config files

--- GateOfOlympus/GateOfOlympus/ViewModel/ThunderViewModel.swift
+++ GateOfOlympus/GateOfOlympus/ViewModel/ThunderViewModel.swift
@@ -28,7 +28,7 @@ struct Grid {
 }
 
 final class ThunderViewModel: ObservableObject {
-    @AppStorage("isOnboarding") var isUserLogin: Bool = true
+    @AppStorage("isOnboarding") var isUserLogin: Bool = false
     @AppStorage("Coins") var coins: Int = 100
     @AppStorage("Hearts") var hearts: Int = 15
     
