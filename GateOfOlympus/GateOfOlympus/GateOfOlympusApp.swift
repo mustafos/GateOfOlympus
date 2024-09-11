f7be21fdc951b6ddea52b530eb32650bb12b4b3d
mustafos <mustafos1999@icloud.com>
2024-09-11 22:52:42 +0200

    feat: rullete example

--- GateOfOlympus/GateOfOlympus/GateOfOlympusApp.swift
+++ GateOfOlympus/GateOfOlympus/GateOfOlympusApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct GateOfOlympusApp: App {
     var body: some Scene {
         WindowGroup {
-            LaunchScreenView()
+//            LaunchScreenView()
+            ExampleView()
         }
     }
 }
